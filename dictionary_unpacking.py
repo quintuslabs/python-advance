6ee7e4c9c5fdf6d8a017a6435f9413444f1e4b58
Sangram <sangram.tat@gmail.com>
2019-02-12 18:07:21 +0530

    added dictionary unpacking,

--- dictionary_unpacking.py
+++ dictionary_unpacking.py
@@ -3,14 +3,15 @@ def display_names(first, second):
 
 names = {"first": "Colt", "second": "Rusty"}
 
-display_names(names) # nope..
+# display_names(names) # nope..
 display_names(**names)  # yup!
 
-def add_and_multiply_numbers(a,b,c,**kwargs):
-    print(a + b * c)
+
+def add_and_multiply_numbers(a, b, c, **kwargs):
+    print(a + b * c , {**kwargs})
     print("OTHER STUFF....")
-    print(kwargs)
+add_and_multiply_numbers(1, 3, 4, name='sangram',z=5)
 
-data = dict(a=1,b=2,c=3,d=55,name="Tony")
-
-add_and_multiply_numbers(**data) # 7
+# data = dict(a=1,b=2,c=3,d=55,name="Tony")
+#
+# add_and_multiply_numbers(**data) # 7
